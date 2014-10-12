83fbfc6e6330335b06b0af2209f1455a89f9959c
Natalia Aksenova <dotokoto@gmail.com>
2014-10-12 21:09:18 +0600

    Заготовка для добавления трамвая
    Изменен config и его использование в TrafficLights, заготовка для
    обработки трамваев

--- tramLights.js
+++ tramLights.js
@@ -2,17 +2,26 @@ var config = {
     red: 3000,
     yellow: 1000,
     green: 5000,
-    setDuration: function (r, y, g) {
+    tramDuration: 10000,
+    tramWaiting: 3000,
+    setDuration: function (r, y, g, d, w) {
         this.red = r;
         this.yellow = y;
         this.green = g;
+        this.tramDuration = d;
+        this.tramWaiting = w;
     }
 };
 
 function TrafficLights(config) {
-    this.config = config;
-    this.state = null;
-    this.timeout = null;
+    this._state = null;
+    this._timeout = null;
+    this.redTime = config.red;
+    this.yellowTime = config.yellow;
+    this.greenTime = config.green;
+    this.tramDuration = config.tramDuration;
+    this.tramWaiting = config.tramWaiting;
+
 }
 
 TrafficLights.prototype.toRed = function () {
@@ -20,7 +29,7 @@ TrafficLights.prototype.toRed = function () {
         clearTimeout(this.timeout);
     }
     this.state = "red";
-    this.timeout = setTimeout(function () {this.toGreen()}.bind(this), this.config.red); 
+    this.timeout = setTimeout(function () {this.toGreen()}.bind(this), this.redTime);
 };
 
 TrafficLights.prototype.toYellow = function () {
@@ -28,7 +37,7 @@ TrafficLights.prototype.toYellow = function () {
         clearTimeout(this.timeout);
     }
     this.state = "yellow";
-    this.timeout = setTimeout(function() {this.toRed()}.bind(this), this.config.yellow);
+    this.timeout = setTimeout(function() {this.toRed()}.bind(this), this.yellowTime);
 };
 
 TrafficLights.prototype.toGreen = function() {
@@ -36,8 +45,13 @@ TrafficLights.prototype.toGreen = function() {
         clearTimeout(this.timeout);
     }
     this.state = "green";
-    this.timeout = setTimeout(function () {this.toYellow()}.bind(this), this.config.green);
+    this.timeout = setTimeout(function () {this.toYellow()}.bind(this), this.greenTime);
 };
+
+TrafficLights.prototype.tramHandler = function() {
+
+};
+
 
 var lights = new TrafficLights(config);
 lights.toYellow();
