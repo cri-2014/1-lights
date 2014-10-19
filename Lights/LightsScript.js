5cdd00439e015afd471b9ecca3dc01e051563e5e
AlexanderVakhitov <deadhunter007@live.ru>
2014-10-19 16:25:34 +0600

    Добавлена обработка события "Трамвай". Все такой же красивый сфетофор :-)

--- Lights/LightsScript.js
+++ Lights/LightsScript.js
@@ -4,12 +4,18 @@ var config =
 	{
         green 	: 6000,
         yellow 	: 2000,
-        red		: 6000,
+        red		: 6000
     },
     order :
 	[
 		'green', 'yellow', 'red'
-	]
+	],
+	tram :
+	{
+		wait  : 3000,
+		time  : 10000,
+		smart : 0.3
+	}
 };
 
 function Lights (config) 
@@ -18,6 +24,10 @@ function Lights (config)
 	this._config 	= config;
 	this._timeout 	= null;
 	this._timeoutID = null;
+	this._lastUse   = null;
+
+	this._prevState = null;
+	this._prevTime  = null;
 								
 	Lights.prototype.state = function() 
 	{
@@ -29,7 +39,7 @@ function Lights (config)
 		var index  = this._config.order.indexOf(this._state);
 		var amount = this._config.order.length;
 
-        var nextColor = this._config.order[ (++index % amount) ];
+        var nextColor = this._config.order[ ( ++index % amount ) ];
         var timeout   = this._config.timeout[ nextColor ];
 		
         this.switchColor(nextColor, timeout);
@@ -39,6 +49,7 @@ function Lights (config)
 	{
 		this._state = color;
 		this._timeout = timeout;
+		this._lastUse = new Date();
 		
 		if (this._timeoutID) 
 		{
@@ -68,12 +79,98 @@ function Lights (config)
 						 this._config.timeout.red);
     };
 	
+    Lights.prototype.tramWaiting = function () 
+    {
+    	if (this._timeoutID) 
+		{
+            clearTimeout(this._timeoutID);
+        }
+        this._timeoutID = setTimeout(function () 
+    	{
+    		this.tramHere();
+    	}.bind(this), this._config.tram.wait);
+	};
+
+	Lights.prototype.tramHere = function () 
+	{
+    	this._prevState = this._state;
+    	this._prevTime  = 
+    		this._config.timeout[this._prevState] - (new Date() - this._lastUse);
+    	this._state     = "green";
+
+    	if (this._timeoutID) 
+		{
+            clearTimeout(this._timeoutID);
+        }
+        this._timeoutID = setTimeout (function () 
+    	{
+    		this.backToLastState();
+    	}.bind(this), this._config.tram.time);
+	};
+
+	Lights.prototype.backToLastState = function () 
+	{
+
+    	if (this._prevTime < this._config.tram.smart * this._config.timeout[this._prevState]) 
+    	{
+    		this._prevTime = 0;
+   	 	}
+   	 	if (this._prevState == "green")
+   	 		if (this._prevTime > this._config.tram.time)
+              	  this._prevTime -= this._config.tram.time;
+
+
+       	this.switchColor(this._prevState, 
+						 this._config.timeout[this._prevState]);
+	};
+
+	Lights.prototype.startListenTram = function(tramEvent) 
+	{
+   		 tramEvent.add("tram", function () 
+   		 {
+   		 	this.tramWaiting()
+   		 }.bind(this));
+	};
+
 	this.toGreen();
+}
+
+function EventEmitter() 
+{
+	this._listeners = {};
+
+	EventEmitter.prototype.add = function(name, func)
+	{
+		if (this._listeners[name] == null)
+			this._listeners[name] = [];
+
+		this._listeners[name].push(func);
+	}
+
+	EventEmitter.prototype.del = function(name, func)
+	{
+		for(var key in _listeners)
+		{
+			for (var i=0; i < this._listeners[key].length; ++i)
+				if (this._listeners[key][i] === func) 
+					this._listeners[key].splice(i, 1);
+		}
+	}
+
+	EventEmitter.prototype.emit = function(key) 
+	{
+		for (var i=0; i < this._listeners[key].length; ++i)
+			setTimeout(this._listeners[key][i], 0);	
+	}
 }
 
 var lights = new Lights(config);
 
-setInterval(function() {
+var tramEvent = new EventEmitter();
+	lights.startListenTram(tramEvent);
+
+setInterval(function() 
+{
 	var currentColor = lights.state();
 	
 	switch(currentColor)
